8a2a2526ec7a816a3e621bd39b0a493ce943392f
analara4 <analarapachecoj@gmail.com>
2025-01-17 12:04:33 -0300

    Atualizando cadastro.cpp
    Implementando os métodos registrarNomeJogador e registrarApelidoJogador

--- cadastro.cpp
+++ cadastro.cpp
@@ -1,1 +1,15 @@
+#include "cadastro.hpp"
 
+std::string Partida::registrarNomeJogador() {
+    std::string nome;
+    std::cout << "Digite seu nome: ";
+    std::getline(std::cin, nome);
+    return nome;
+}
+
+std::string Partida::registrarApelidoJogador() {
+    std::string apelido;
+    std::cout << "Digite seu apelido: ";
+    std::cin >> apelido;
+    return apelido;
+}
