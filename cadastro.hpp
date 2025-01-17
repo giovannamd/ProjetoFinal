2de498db067f493d7caa66f2ce5206ac8060a6ba
analara4 <analarapachecoj@gmail.com>
2025-01-17 11:53:05 -0300

    Atualizando cadastro.hpp
    Criando a classe Jogador, para as informações do jogador

--- cadastro.hpp
+++ cadastro.hpp
@@ -3,3 +3,17 @@
 // listagem de jogadores (LJ [A|N]): mostrar todos os jogadores com seus respectivos números de vitórioas e derrotas em cada jogo
 // início de partida (EP <Jogo: (R|L|V)> <Apelido Jogador 1> <Apelido Jogador 2>): iniciar uma partida entre dois jogadores em um dos jogos
 // finalização do sistema (FS): encerrar a execução do sistema
+
+// Informações do jogador
+class Jogador {
+public:
+    std::string nome;
+    std::string apelido;
+
+    int vitoriasReversi = 0;
+    int derrotasReversi = 0;
+    int vitoriasLig4 = 0;
+    int derrotasLig4 = 0;
+    int vitoriasJogoVelha = 0;
+    int derrotasJogoVelha = 0;
+};
