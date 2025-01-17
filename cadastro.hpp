2e374f9c0ed5ddb20cc59ce73086c2a35e4803c8
analara4 <analarapachecoj@gmail.com>
2025-01-17 11:49:09 -0300

    Criando cadastro.hpp
    Criando um módulo para cuidar do cadastro de jogadores.

---
+++ cadastro.hpp
@@ -0,0 +1,5 @@
+// cadastro de jogadores (CJ <Apelido> <Nome>): adicionar novos jogadores pelo nome e apelido único
+// remoção de jogadores (RJ <Apelido>): remover jogadores existentes
+// listagem de jogadores (LJ [A|N]): mostrar todos os jogadores com seus respectivos números de vitórioas e derrotas em cada jogo
+// início de partida (EP <Jogo: (R|L|V)> <Apelido Jogador 1> <Apelido Jogador 2>): iniciar uma partida entre dois jogadores em um dos jogos
+// finalização do sistema (FS): encerrar a execução do sistema
